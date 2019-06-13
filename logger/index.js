cceb3d3279c424e96884cf15ac18c6a02abdd35c
Joey Ferris <joey@joeyferris.io>
2019-06-13 16:00:31 -0500

    Fixing formatting issues for logger module

--- logger/index.js
+++ logger/index.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict';
 
-const chalk = require("chalk");
+const chalk = require('chalk');
 
 const log = console.log;
 
 const errorLogger = chalk.bold.red;
-const warningLogger = chalk.keyword("orange");
-const infoLogger = chalk.underline.bold.keyword("blue");
+const warningLogger = chalk.keyword('orange');
+const infoLogger = chalk.underline.bold.keyword('blue');
 
 module.exports = {
   log,
   errorLogger,
   warningLogger,
-  infoLogger
+  infoLogger,
 };
