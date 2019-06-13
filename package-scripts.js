cac09e3163a0845f28c9dcb2d45a72c91e0aeb96
Joey Ferris <joey@joeyferris.io>
2019-06-13 15:40:13 -0500

    Updated prettierrc and package-scripts

--- package-scripts.js
+++ package-scripts.js
@@ -1,43 +1,41 @@
-"use strict";
+'use strict';
 
-const npsUtils = require("nps-utils");
+const npsUtils = require('nps-utils');
 
 module.exports = {
   scripts: {
     default: {
-      script: "node server.js",
-      description: "runs server"
+      script: 'node server.js',
+      description: 'runs server',
     },
     dev: {
       default: {
-        script: "nodemon server.js",
-        description:
-          "runs server using nodemon for restart server on file change"
-      }
+        script: 'nodemon server.js',
+        description: 'runs server using nodemon for restart server on file change',
+      },
     },
     test: 'echo "Error: no test specified" && exit 1',
     prettier: {
       default: {
-        script: "nps prettier.quick",
-        description: "runs pretty quick on all files staged for commit"
+        script: 'nps prettier.quick',
+        description: 'runs pretty quick on all files staged for commit',
       },
       watch: {
-        script:
-          'onchange "**/*.js" -- prettier --config .prettier.json *.js --write {{changed}}',
-        description: "runs prettier on change of file"
+        script: 'onchange "**/*.js" -- prettier --config .prettierrc.json *.js --write {{changed}}',
+        description: 'runs prettier on change of file',
       },
       fix: {
-        script: "prettier --config .prettier.json *.js --write",
-        description: "runs prettier on all source files"
+        script: 'prettier --config .prettierrc.json *.js --write',
+        description: 'runs prettier on all source files',
       },
       quick: {
-        script: "pretty-quick --staged",
-        description: "runs prettier formatting on all staged files"
-      }
+        script: 'pretty-quick --staged',
+        description: 'runs prettier formatting on all staged files',
+      },
     },
     // TODO: run lint, test scripts
     // returns 'ng lint && ng test'
     // validate: npsUtils.series.nps('lint', 'test --coverage'),
-    validate: npsUtils.series.nps("prettier.quick")
-  }
+    validate: npsUtils.series.nps('prettier.quick'),
+  },
 };
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const logger = require("./logger");
+const express = require('express');
+const logger = require('./logger');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (request, response) => {
-  return response.send("OK");
+app.get('/', (request, response) => {
+  return response.send('OK');
 });
 
-app.listen(PORT, error => {
+app.listen(PORT, (error) => {
   if (error) {
-    logger.log(logger.errorLogger("Something is wrong..."));
+    logger.log(logger.errorLogger('Something is wrong...'));
   } else {
     logger.log(logger.infoLogger(`\nServer started on ${PORT}`));
   }
